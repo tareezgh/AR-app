911b8f7a1af798a937b8b2bd595eeec00ae460b8
Tareez Ghandour <tareezghandour@hotmail.com>
2023-06-17 18:12:31 +0300

    update scenes

--- Assets/Scripts/IconController.cs
+++ Assets/Scripts/IconController.cs
@@ -5,18 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class IconController : MonoBehaviour
 {
-    public void Play()
+
+    public void LoadOpening()
     {
-        Debug.Log("Main scene");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("Opening scene");
+        SceneManager.LoadScene(0); 
     }
 
-    public void Back()
+    public void LoadText()
     {
-        Debug.Log("Secondary scene");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        Debug.Log("Text scene");
+        SceneManager.LoadScene(1); 
     }
 
+    public void LoadWar()
+    {
+        Debug.Log("War scene");
+        SceneManager.LoadScene(2); 
+    }
 
-  
+    public void LoadMap()
+    {
+        Debug.Log("Map scene");
+        SceneManager.LoadScene(3); 
+    }
+
 }
--- Assets/Scripts/TextSceneController.cs
+++ Assets/Scripts/TextSceneController.cs
@@ -3,30 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro; // Add this line if using TextMeshPro
 
-public class MainSceneController : MonoBehaviour
+public class TextSceneController : MonoBehaviour
 {
     public TextMeshProUGUI textMeshPro; 
-
-     public TextMeshProUGUI textSyrian; 
-      public TextMeshProUGUI textIsrael; 
     public AudioSource audioSource;
 
-     public GameObject Tanks;
-    
+
     void Start()
     {
-        Tanks.SetActive(false);
         textMeshPro.font = Resources.Load<TMP_FontAsset>("Fonts/HebrewFont");
         textMeshPro.alignment = TextAlignmentOptions.Right;
         
         
         textMeshPro.text = "שלום אתם נמצאים באנדרטאת חטבית השריון 971 האנדרטה הוקמה על מנת להנציח את לחימת הגבורה של החטיבה במלחמת יום הכיפורים, חטיבה 971 הוקמה באמצע שנות ה-05 של המאה הקודמת והייתה אחת משתי חטיבות השריון במילואים הראשונות של צה״ל החטיבה השתתפה במבצע קדש בסיני ובמלחמת ששת הימים בשמרון ובגולן";
 
-        textSyrian.gameObject.SetActive(false);
-        textIsrael.gameObject.SetActive(false);
+    
     }
 
-     void Update()
+    void Update()
     {
         if (audioSource.isPlaying)
         {
@@ -35,14 +29,7 @@ public class MainSceneController : MonoBehaviour
             int currentWordIndex = Mathf.FloorToInt(playbackPercentage * textMeshPro.textInfo.wordCount);
             textMeshPro.maxVisibleWords = currentWordIndex + 1;
         }
-        else
-        {
-            // The audio has stopped, hide the text
-            textMeshPro.maxVisibleWords = 0;
-            Tanks.SetActive(true);
-            textSyrian.gameObject.SetActive(true);
-            textIsrael.gameObject.SetActive(true);
-        }
+        
     }
 
     
